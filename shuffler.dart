9a944519678fbb9272af1f9e1672c8db4c8f1b11
Cody Ko In A Box <codykoinabox@gmail.com>
2024-03-19 22:05:46 -0300

    we might need to change the random number's seed in the future

---
+++ shuffler.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+class Carta {
+  var numero;
+  var naipe;
+
+  Carta(this.numero, this.naipe);
+
+  @override
+  String toString() => '(numero: $numero, naipe $naipe)';
+}
+
+List<Carta> generateDeck(int number_of_numeros, int number_of_naipes){
+
+  List<Carta> deck = [];
+
+ for(var i = 0; i < number_of_numeros; i++){
+    for(var y = 0; y < number_of_naipes; y++){
+      deck.add(new Carta(i, y));
+    }
+  }
+
+  return deck;
+
+}
+
+List<Carta> shuffleDeck(List<Carta> deck){
+  
+  List<Carta> shuffledDeck = [];
+  Random random = new Random(DateTime.now().microsecondsSinceEpoch);
+
+  for(var i = 0; i < deck.length; i++){
+    shuffledDeck.add(deck[random.nextInt(deck.length)]);
+  }
+
+  return shuffledDeck;
+
+}
+
+
+void main() {
+
+  var deck = generateDeck(10, 4);
+
+  var shuffled = shuffleDeck(deck);
+
+  for(int i = 0; i < shuffled.length; i++){
+    print(shuffled[i]);
+  }
+
+}
